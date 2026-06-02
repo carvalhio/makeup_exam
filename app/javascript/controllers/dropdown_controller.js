1ae0be62adbb8c2132a477c40992c9410aff03be
carvalhio <raffael.carvallho@gmail.com>
2026-06-02 13:33:11 -0300

    print button created

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,17 +1,30 @@
-// app/javascript/controllers/dropdown_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["menu"]
 
   connect() {
-    this.open = false
+    this.boundClose = this.closeOnOutsideClick.bind(this)
+    document.addEventListener("click", this.boundClose)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.boundClose)
   }
 
   toggle(event) {
     event.preventDefault()
-    this.open = !this.open
+    event.stopPropagation()
 
-    this.menuTarget.style.display = this.open ? "block" : "none"
+    this.menuTarget.style.display =
+      this.menuTarget.style.display === "none"
+        ? "block"
+        : "none"
+  }
+
+  closeOnOutsideClick(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.style.display = "none"
+    }
   }
 }
