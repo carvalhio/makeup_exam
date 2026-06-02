195ea6f477cff164c14f77e716aea8aa51567aa9
carvalhio <raffael.carvallho@gmail.com>
2026-06-02 17:28:28 -0300

    attendanceList oder by school class level

--- app/controllers/exam_periods_controller.rb
+++ app/controllers/exam_periods_controller.rb
@@ -1,48 +1,33 @@
 class ExamPeriodsController < ApplicationController
   before_action :set_exam_period, only: %i[ show edit update destroy ]
 
-  # GET /exam_periods or /exam_periods.json
+  # GET /exam_periods
   def index
     @exam_periods = ExamPeriod.all
   end
 
-  # GET /exam_periods/1 or /exam_periods/1.json
+  # GET /exam_periods/1
   def show
-  @exam_period = ExamPeriod.find(params[:id])
+    @exam_period = ExamPeriod.find(params[:id])
 
-  @exam_requests =
-    @exam_period.exam_requests
-                .includes(student: :school_class)
-                .includes(:subjects)
-                .sort_by do |request|
-      school_class = request.student.school_class
+    @exam_requests =
+      @exam_period.exam_requests
+                  .includes(student: :school_class)
+                  .includes(:subjects)
+                  .sort_by do |request|
+        school_class = request.student.school_class
 
-      grade_order =
-        case school_class.grade
-        when "1º ano" then 1
-        when "2º ano" then 2
-        when "3º ano" then 3
-        when "4º ano" then 4
-        when "5º ano" then 5
-        when "6º ano" then 6
-        when "7º ano" then 7
-        when "8º ano" then 8
-        when "9º ano" then 9
-        when "1ª série" then 10
-        when "2ª série" then 11
-        when "3ª série" then 12
-        else 99
-        end
+        [
+          grade_order_value(school_class.grade),
+          school_class.identifier
+        ]
+      end
 
-      [ grade_order, school_class.identifier ]
-    end
-
-  @requests_grouped =
-    @exam_requests.group_by do |request|
-      school_class = request.student.school_class
-
-      "#{school_class.grade} #{school_class.identifier}"
-    end
+    @requests_grouped =
+      @exam_requests.group_by do |request|
+        school_class = request.student.school_class
+        "#{school_class.grade} #{school_class.identifier}"
+      end
   end
 
   # GET /exam_periods/new
@@ -54,7 +39,7 @@ class ExamPeriodsController < ApplicationController
   def edit
   end
 
-  # POST /exam_periods or /exam_periods.json
+  # POST /exam_periods
   def create
     @exam_period = ExamPeriod.new(exam_period_params)
 
@@ -69,7 +54,7 @@ class ExamPeriodsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /exam_periods/1 or /exam_periods/1.json
+  # PATCH/PUT /exam_periods/1
   def update
     respond_to do |format|
       if @exam_period.update(exam_period_params)
@@ -82,63 +67,68 @@ class ExamPeriodsController < ApplicationController
     end
   end
 
+  # PRINT MAP
   def print_map
-  @exam_period = ExamPeriod.find(params[:id])
+    @exam_period = ExamPeriod.find(params[:id])
 
-  items = []
+    items = []
 
-  @exam_period.exam_requests
-            .includes(:subjects)
-            .includes(student: :school_class)
-            .find_each do |request|
-  school_class = request.student.school_class
+    @exam_period.exam_requests
+                .includes(:subjects)
+                .includes(student: :school_class)
+                .find_each do |request|
+      school_class = request.student.school_class
+      application_shift = request.application_shift
 
-  application_shift = request.application_shift
-puts "ALUNO: #{request.student.name} | TURMA: #{school_class.shift} | APLICAÇÃO: #{application_shift} | SAME_SHIFT: #{request.same_shift}"
-  request.subjects.each do |subject|
-    items << {
-      shift: application_shift,
-      grade: school_class.grade,
-      subject: subject.name
-    }
-  end
-end
-grade_order = {
-  "1º ano" => 1,
-  "2º ano" => 2,
-  "3º ano" => 3,
-  "4º ano" => 4,
-  "5º ano" => 5,
-  "6º ano" => 6,
-  "7º ano" => 7,
-  "8º ano" => 8,
-  "9º ano" => 9,
-  "1ª série" => 10,
-  "2ª série" => 11,
-  "3ª série" => 12
-}
+      puts "ALUNO: #{request.student.name} | TURMA: #{school_class.shift} | APLICAÇÃO: #{application_shift} | SAME_SHIFT: #{request.same_shift}"
 
-shift_order = {
-  "Manhã" => 1,
-  "Tarde" => 2
-}
-
-@print_map =
-  items.group_by { |i| i[:shift] }
-       .sort_by { |shift, _| shift_order[shift] || 999 }
-       .to_h
-       .transform_values do |shift_items|
-    shift_items.group_by { |i| i[:grade] }
-               .sort_by { |grade, _| grade_order[grade] || 999 }
-               .to_h
-               .transform_values do |grade_items|
-      grade_items.group_by { |i| i[:subject] }
-                 .transform_values(&:count)
+      request.subjects.each do |subject|
+        items << {
+          shift: application_shift,
+          grade: school_class.grade,
+          subject: subject.name
+        }
+      end
     end
-  end
-end
 
-  # DELETE /exam_periods/1 or /exam_periods/1.json
+    shift_order = {
+      "Manhã" => 1,
+      "Tarde" => 2
+    }
+
+    @print_map =
+      items.group_by { |i| i[:shift] }
+           .sort_by { |shift, _| shift_order[shift] || 999 }
+           .to_h
+           .transform_values do |shift_items|
+        shift_items.group_by { |i| i[:grade] }
+                   .sort_by { |grade, _| grade_order_value(grade) }
+                   .to_h
+                   .transform_values do |grade_items|
+          grade_items.group_by { |i| i[:subject] }
+                     .transform_values(&:count)
+        end
+      end
+  end
+
+  # ATTENDANCE LIST
+  def attendance_list
+    @exam_period = ExamPeriod.find(params[:id])
+
+    requests = @exam_period
+                 .exam_requests
+                 .includes(:subjects, student: :school_class)
+
+    @morning_requests = requests
+      .select { |r| r.application_shift == "Manhã" }
+      .sort_by { |r| [ grade_order_value(r.student.school_class.grade), r.student.name ] }
+
+    @afternoon_requests = requests
+      .select { |r| r.application_shift == "Tarde" }
+      .sort_by { |r| [ grade_order_value(r.student.school_class.grade), r.student.name ] }
+  end
+
+  # DELETE /exam_periods/1
   def destroy
     @exam_period.destroy!
 
@@ -148,28 +138,32 @@ end
     end
   end
 
-  def attendance_list
-  @exam_period = ExamPeriod.find(params[:id])
+  private
 
-  requests = @exam_period
-               .exam_requests
-               .includes(:subjects, student: :school_class)
+  def set_exam_period
+    @exam_period = ExamPeriod.find(params[:id])
+  end
 
-    @morning_requests = requests.select do |request|
-      request.application_shift == "Manhã"
-    end
-     @afternoon_requests = requests.select do |request|
-       request.application_shift == "Tarde"
+  def exam_period_params
+    params.require(:exam_period).permit(:stage, :exam_type, :active)
+  end
+
+  # CENTRAL GRADE ORDER
+  def grade_order_value(grade)
+    case grade
+    when "1º ano" then 1
+    when "2º ano" then 2
+    when "3º ano" then 3
+    when "4º ano" then 4
+    when "5º ano" then 5
+    when "6º ano" then 6
+    when "7º ano" then 7
+    when "8º ano" then 8
+    when "9º ano" then 9
+    when "1ª série" then 10
+    when "2ª série" then 11
+    when "3ª série" then 12
+    else 99
     end
   end
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_exam_period
-      @exam_period = ExamPeriod.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def exam_period_params
-      params.require(:exam_period).permit(:stage, :exam_type, :active)
-    end
 end
--- app/helpers/exam_periods_helper.rb
+++ app/helpers/exam_periods_helper.rb
@@ -1,21 +1,20 @@
 module ExamPeriodsHelper
-end
-
-def subject_abbreviation(subject_name)
-  {
-    "Português" => "PORT",
-    "Matemática" => "MAT",
-    "História" => "HIS",
-    "Geografia" => "GEO",
-    "Ciências" => "CIE",
-    "Arte" => "ART",
-    "Literatura" => "LIT",
-    "Inglês" => "ING",
-    "Redação" => "RED",
-    "Física" => "FIS",
-    "Química" => "QUI",
-    "Biologia" => "BIO",
-    "Filosofia" => "FIL",
-    "Sociologia" => "SOC"
-  }[subject_name] || subject_name.first(4).upcase
+  def subject_abbreviation(name)
+    {
+      "Português" => "PORT",
+      "Matemática" => "MAT",
+      "História" => "HIS",
+      "Geografia" => "GEO",
+      "Ciências" => "CIE",
+      "Arte" => "ART",
+      "Literatura" => "LIT",
+      "Inglês" => "ING",
+      "Redação" => "RED",
+      "Física" => "FIS",
+      "Química" => "QUI",
+      "Biologia" => "BIO",
+      "Filosofia" => "FIL",
+      "Sociologia" => "SOC"
+    }[name] || name.first(4).upcase
+  end
 end
