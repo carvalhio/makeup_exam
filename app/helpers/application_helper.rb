de2fe8656951dcfeaff304f2a5f3119f1a421d86
carvalhio <raffael.carvallho@gmail.com>
2026-09-09 16:22:15 -0300

    try to display ordinal symbol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def ordinal_pt(numero, genero = :masc)
+    simbolo = genero == :fem ? "ª" : "º"
+    # força a renderização como ordinal com sup
+    "#{numero}<sup class='ordinal-indicator'>#{simbolo}</sup>".html_safe
+  end
 end
